a3f66d19ba18b3f380f63dc3289b1de4aafd9a3f
xiaowu luo <luoxiaowu2012@gmail.com>
2019-05-23 17:24:04 +0800

    [xiaowu] commit file FirstActivity.java

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -2,6 +2,9 @@ package com.example.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import static com.example.activitytest.R.layout.layout;
 
@@ -11,7 +14,13 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout);
-
+        Button button1=(Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "you click a button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 

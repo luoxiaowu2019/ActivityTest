0a1e70e42ebdd5a28ab2ea38c9712e6f25226737
xiaowu luo <luoxiaowu2012@gmail.com>
2019-05-23 16:47:22 +0800

    [xiaowu] "AndroidManifest.xml","FirstActivity.java","layout.xml"    commit

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -3,11 +3,15 @@ package com.example.activitytest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static com.example.activitytest.R.layout.layout;
+
 public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(layout);
+
     }
 }
 
